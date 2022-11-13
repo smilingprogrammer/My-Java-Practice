678fcdc39c737dc62f1ed4f3fbae046aafae8386
Oyewale Abdulsobur <66224527+smilingprogrammer@users.noreply.github.com>
2022-11-13 17:50:26 +0100

    Solution to if else assignment

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -33,20 +33,47 @@ public class Main {
 //        System.out.println("Total of everything is "+wholeNumber+"The season is: "+ season + "The adjective is" + adjective);
 
         // If statement
-        System.out.println("Sales number");
+//        System.out.println("Sales number");
+//        Scanner scanner = new Scanner(System.in);
+//        int sales = scanner.nextInt();
+//        scanner.close();
+//
+//        int bonus = 250;
+//        int salary = 200;
+//        int minSale = 9;
+//
+//        if(sales > minSale){
+//            salary = salary + bonus;
+//            System.out.println("Your Total salary is" + salary);
+//        } else {
+//            System.out.println("No salary available for you");
+//        }
+
+        // Assignment of if statements
+
+        System.out.println("Pennies");
         Scanner scanner = new Scanner(System.in);
-        int sales = scanner.nextInt();
+        double pennies = scanner.nextDouble();
+
+        System.out.println("nickel");
+        double nickel = scanner.nextDouble();
+
+        System.out.println("dime");
+        double dime = scanner.nextDouble();
+
+        System.out.println("quarter");
+        double quarter = scanner.nextDouble();
+
+        double total = pennies + nickel + dime + quarter;
         scanner.close();
+        int exactNumberExpected = 1;
 
-        int bonus = 250;
-        int salary = 200;
-        int minSale = 9;
-
-        if(sales > minSale){
-            salary = salary + bonus;
-            System.out.println("Your Total salary is" + salary);
+        if (total == exactNumberExpected){
+            System.out.println("You win");
+        } else if (total > exactNumberExpected){
+            System.out.println("You went over");
         } else {
-            System.out.println("No salary available for you");
+            System.out.println("You went under");
         }
     }
 }
