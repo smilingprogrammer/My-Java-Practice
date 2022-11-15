e9e189eeff41499e2ab689b64520907a4b86434f
Oyewale Abdulsobur <66224527+smilingprogrammer@users.noreply.github.com>
2022-11-15 14:02:16 +0100

    While Loop

--- src/com/company/MyLoop.java
+++ src/com/company/MyLoop.java
@@ -1,4 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class MyLoop {
+
+    public static void main(String[] args){
+
+        int rate = 10;
+        int maxHours = 20;
+        Scanner scanner = new Scanner(System.in);
+        double hoursWorked = scanner.nextDouble();
+
+        while (hoursWorked > maxHours || hoursWorked < 1){
+            System.out.println("Not Verified");
+            hoursWorked = scanner.nextDouble();
+        }
+
+        double salary = rate * hoursWorked;
+        System.out.println("Verified, your pay is $" + salary);
+    }
 }
