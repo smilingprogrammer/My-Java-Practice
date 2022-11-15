e9da284f7287c6df51306dc3100a8b76400c1eba
Oyewale Abdulsobur <66224527+smilingprogrammer@users.noreply.github.com>
2022-11-15 14:17:17 +0100

    Do-While loop

--- src/com/company/MyLoop.java
+++ src/com/company/MyLoop.java
@@ -6,17 +6,38 @@ public class MyLoop {
 
     public static void main(String[] args){
 
-        int rate = 10;
-        int maxHours = 20;
+//        int rate = 10;
+//        int maxHours = 20;
+//        Scanner scanner = new Scanner(System.in);
+//        double hoursWorked = scanner.nextDouble();
+//
+//        while (hoursWorked > maxHours || hoursWorked < 1){
+//            System.out.println("Not Verified");
+//            hoursWorked = scanner.nextDouble();
+//        }
+//
+//        double salary = rate * hoursWorked;
+//        System.out.println("Verified, your pay is $" + salary);
+//        scanner.close()
+
+
         Scanner scanner = new Scanner(System.in);
-        double hoursWorked = scanner.nextDouble();
+        boolean repeat;
 
-        while (hoursWorked > maxHours || hoursWorked < 1){
-            System.out.println("Not Verified");
-            hoursWorked = scanner.nextDouble();
-        }
+        do{
+            System.out.println("Enter yor first number");
+            int num1 = scanner.nextInt();
 
-        double salary = rate * hoursWorked;
-        System.out.println("Verified, your pay is $" + salary);
+            System.out.println("Enter your second number");
+            int num2 = scanner.nextInt();
+
+            int total = num1 + num2;
+            System.out.println("Sum of both number is "+total);
+
+            System.out.println("Do you want to try again?");
+            repeat = scanner.nextBoolean();
+        } while (repeat);
+
+        scanner.close();
     }
 }
