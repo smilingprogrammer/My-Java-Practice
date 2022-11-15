c549c39d0bba4b57112c3320d0015859b465e264
Oyewale Abdulsobur <66224527+smilingprogrammer@users.noreply.github.com>
2022-11-15 15:11:32 +0100

    For loop char not working

--- src/com/company/MyLoop.java
+++ src/com/company/MyLoop.java
@@ -21,23 +21,47 @@ public class MyLoop {
 //        scanner.close()
 
 
+//        Scanner scanner = new Scanner(System.in);
+//        boolean repeat;
+//
+//        do{
+//            System.out.println("Enter yor first number");
+//            int num1 = scanner.nextInt();
+//
+//            System.out.println("Enter your second number");
+//            int num2 = scanner.nextInt();
+//
+//            int total = num1 + num2;
+//            System.out.println("Sum of both number is "+total);
+//
+//            System.out.println("Do you want to try again?");
+//            repeat = scanner.nextBoolean();
+//        } while (repeat);
+//
+//        scanner.close();
+
+        // For loop
+
         Scanner scanner = new Scanner(System.in);
-        boolean repeat;
-
-        do{
-            System.out.println("Enter yor first number");
-            int num1 = scanner.nextInt();
-
-            System.out.println("Enter your second number");
-            int num2 = scanner.nextInt();
-
-            int total = num1 + num2;
-            System.out.println("Sum of both number is "+total);
-
-            System.out.println("Do you want to try again?");
-            repeat = scanner.nextBoolean();
-        } while (repeat);
-
+        System.out.println("Enter a number to search for letter D");
+        String letter = scanner.next();
         scanner.close();
+
+        boolean letterFound = false;
+
+        for (int i = 0; i < letter.length(); i++){
+            char currentLetter = letter.charAt(i);
+
+            if (currentLetter == "D" || currentLetter == "d"){
+                letterFound = true;
+                break;
+            }
+        }
+
+        if (letterFound){
+            System.out.println("Letter D found");
+        } else {
+            System.out.println("Letter D not found");
+        }
     }
 }
