20e077c7e999f51466a3f1f09b97e415ffb42787
Oyewale Abdulsobur <66224527+smilingprogrammer@users.noreply.github.com>
2022-12-05 13:30:22 +0100

    Buggy

---
+++ src/com/company/String.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class String {
+
+    Scanner scanner = new Scanner(System.in);
+    int LENGTH =  8;
+    public static void main(String[] args){
+
+    }
+
+    public static String password(String password){
+
+    }
+}
