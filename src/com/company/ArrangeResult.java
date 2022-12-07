34d2b061b5cb152fe95f5ec9dcf784885616a524
Oyewale Abdulsobur <66224527+smilingprogrammer@users.noreply.github.com>
2022-12-07 15:24:44 +0100

    Assessment not solved

---
+++ src/com/company/ArrangeResult.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Map;
+
+public class ArrangeResult extends CollectionLoop{
+
+    public static void main(String[] args){
+
+        Map getGrades = getOriginalGrades();
+        Map getMakeUp = getMakeUpGrades();
+
+//        Map entry = getGrades.entrySet();
+
+    }
+}
